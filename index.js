7c29c4b193fd6842368002e27663545c22fbc422
Alexis <alexisbou16@gmail.com>
2023-05-06 21:12:59 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  }
+
+function writeCards(arrayOfStringNames, eventName){
+    const newArray = []
+    for(let i = 0; i < arrayOfStringNames.length; i++){
+        newArray.push(`Thank you, ${arrayOfStringNames[i]}, for the wonderful surprise gift!`);
+    }
+    return newArray;
+}
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number--)
+    }
+}
